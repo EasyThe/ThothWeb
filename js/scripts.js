f400531adec2c853acfa575f1884fb021eb1045b
Seyhan Halil <easythebg@gmail.com>
2020-04-20 03:06:25 +0300

    Testing script, changed slightly the header height

--- js/scripts.js
+++ js/scripts.js
@@ -197,6 +197,34 @@
 	});
     
 
+    var getJSON = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.open('GET', url, true);
+        xhr.withCredentials = true;
+        xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+          var status = xhr.status;
+          if (status === 200) {
+            callback(null, xhr.response);
+          } else {
+            callback(status, xhr.response);
+          }
+        };
+        xhr.send();
+    };
+    $(document).ready(getJSON('https://pastebin.com/raw/gZZNpAYP',
+    function(err, data) {
+      if (err !== null) {
+        alert('Something went wrong: ' + err);
+      } else {
+        alert('Your query count: ' + data.query.count);
+      }
+    }));
+
+
     /* Counter - CountTo */
 	var a = 0;
 	$(document).ready(function() {
